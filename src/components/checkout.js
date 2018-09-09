6daa2a359759b6929d9c31c5cec7c3985775690c
Giu Magnani <glmagnani@gmail.com>
2018-09-09 23:10:46 +0200

    remove some comments

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -41,8 +41,6 @@ const Checkout = class extends React.Component {
 
   componentDidMount() {
     this.stripeHandler = StripeCheckout.configure({
-      // You’ll need to add your own Stripe public key to the `checkout.js` file.
-      // key: 'pk_test_STRIPE_PUBLISHABLE_KEY',
       key: process.env.stripe_key,
       closed: () => {
         this.resetButton();
